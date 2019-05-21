d367d683ae8cd7a516005d686a2553602b157c0b
willie capre <wcap18@gmail.com>
2019-05-20 21:31:04 -0400

    added remove
    I added the remove and add active.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 /*** 
    This are the variables that are stored and will be needed to 
@@ -17,8 +16,7 @@ const listItem = document.querySelectorAll(".student-item");
 const perPage = 10;
 var pageDiv = document.querySelector(".page");
 
-
-console.log (listItem);
+console.log(listItem);
 
 /***
 Class.addEventListener ('click')() = > {
@@ -26,142 +24,129 @@ p.textContext = Input.value + ';';
 }
 ***/
 
-
-
 /*** 
    Create a showPage function to hide all items  in the
    list except for the ten that needs to be shown.
 ***/
 
 function showPage(page, list) {
-   console.log("function is running");
+  console.log("function is running");
 
-  var startIndex = (page * perPage) - perPage;
+  var startIndex = page * perPage - perPage;
   var endIndex = page * perPage;
 
-  for(var i=0; i < list.length; i++) {
+  for (var i = 0; i < list.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      list[i].style.display = "";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
+}
 
-      if (i >= startIndex && i < endIndex) {
+showPage(1, listItem);
 
-       list[i].style.display = '';
-
-      } else{
-       list[i].style.display = 'none'; 
-
-      }
-     }
-   };
-
-  //showPage(1, listItem);
-
-   /***
+/***
     we will create an appendPageLinks function to  generate, append, 
     and add the pagination buttons.
    ***/
-  
 
-   function appendPageLinks (list) {
-      totalPageNum = Math.ceil(list.length / 10);
-      //console.log(totalPage);
+function appendPageLinks(list) {
+  totalPageNum = Math.ceil(list.length / 10);
+  //console.log(totalPage);
 
-      var paginationDiv = document.createElement("div");
-      pageDiv.appendChild(paginationDiv);
-      var paginationUl = document.createElement("ul");
-      paginationDiv.appendChild(paginationUl);
+  var paginationDiv = document.createElement("div");
+  pageDiv.appendChild(paginationDiv);
+  paginationDiv.className = "pagination";
+  var paginationUl = document.createElement("ul");
+  paginationDiv.appendChild(paginationUl);
 
-      paginationDiv.className = "pagination";
-     
-      console.log(pageDiv);
+  console.log(pageDiv);
 
-
-/*** This area will create li and a element, also adding href 
+  /*** This area will create li and a element, also adding href 
  to the page and start with the fisrt page.
  ***/
-      for (var i = 0; i < totalPageNum ; i++) {
-        const a = document.createElement("a"); 
-        const li = document.createElement("li"); 
-        a.href = "#"; 
-        a.textContent = i + 1;  
-        li.appendChild = document.createElement ("a");
-        paginationUl.appendChild(li); 
+  for (var i = 0; i < totalPageNum; i++) {
+    const a = document.createElement("a");
+    const li = document.createElement("li");
+    a.href = "#";
+    a.textContent = i + 1;
+    li.appendChild(a);
+    paginationUl.appendChild(li);
 
-         //console.log(a);
+    //console.log(a);
 
-        //var totalPage = document.querySelectorAll("paginationLinks"); //"active";
-       // console.log(totalPage);
-         var totalPage = document.getElementsByTagName("a");
+    //var totalPage = document.querySelectorAll("paginationLinks"); //"active";
+    // console.log(totalPage);
 
-       if ( i === 0 ){
-        pagination.classList = ("active");
-       }
-        /***
+    //var totalPage = document.getElementsByTagName("a");
+    //var paragraph = document.getElementsByTagName("div");
+
+    //for (i = 0 ; i < paragraph.length; ++i){
+    if (i === 0) {
+      a.classList = "active";
+    }
+    /***
          add the event listener into the pagination links
+
          ***/
-        totalPage[i].addEventListener("click", (event) => {
-         let totalPageNum = event.target.innerText;
+    a.addEventListener("click", event => {
+      let totalPageNum = event.target.innerText;
 
-         showPage(i,listItem);
+      showPage(totalPageNum, listItem);
 
-         if (event.target.className= "A");
+      if ((event.target.className = "A"));
 
-        var totalPage = document.querySelectorAll("paginationLinks");
+      var totalPage = document.querySelectorAll("paginationLinks");
 
-         for (let i=0; i < totalPage; i++) { 
-         totalPageNum.className = event.target.textContext;
-         event.target.classList.add("active");
+      //The active class name should be removed from all pagination links
 
-         for (let i = 0; i < totalPage.length; i += 1) {ß
-         appendPageLinks[i].classList.remove("active");
-         //event.target.classList.add("active");
-        }
-         }
-      });
+      for (let i = 0; i < totalPage.length; i += 1) {
+        appendPageLinks[i].classList.remove("active");
       }
-     
-         console.log("totalPageNum");
-      
-      const activeClassName = {
-         add: (eventListener) => event.activeClassName = "active",
-         remove: (page) => {
-         
-      //for (var i = 0; i < a.length; i++) {
-        // let active = document.querySelectorAll("paginationLinks");
-         //active[i].activeClassName='';
 
-      
-         //if (pageDiv.style.display == "none") {
-         
-         
-         //} else {
-           // pageDiv.style.display = "block"; 
-           // addItemPage.addEventListener ("click");
-             //  listLi = document.createElement ("li");
-               //liTextContext = addItemInput.value;
+      //The active class name should be added to the link that was just clicked.
+      //for (let i = 0; i < totalPage; i++) {
+      totalPageNum.className = event.target.textContext;
+      event.target.classList.add("active");
+      //}
+    });
+  }
 
-              // for (var i = 0; i < a.length; i++) {
-               
-            
-            //}
+  //console.log("totalPageNum");
 
-            
-          //  function totalPageNum() {
-             //  return Math.ceil(list.length / totalPageNumber);
-            //for (var i = 0; i < PerPage; i++) {
-              //const li = document.getElement("li");
-               //const a = document.getElement("a");
-              // a.textContext = i;
-               //li.appendChild= document.createElement ("a");
-              // ul.appendChild(li); 
+  //const activeClassName = {
+  // add: eventListener => (event.activeClassName = "active"),
+  //remove: page => {
+  //for (var i = 0; i < a.length; i++) {
+  // let active = document.querySelectorAll("paginationLinks");
+  //active[i].activeClassName='';
+  //if (pageDiv.style.display == "none") {
+  //} else {
+  // pageDiv.style.display = "block";
+  // addItemPage.addEventListener ("click");
+  //  listLi = document.createElement ("li");
+  //liTextContext = addItemInput.value;
+  // for (var i = 0; i < a.length; i++) {
+  //}
+  //  function totalPageNum() {
+  //  return Math.ceil(list.length / totalPageNumber);
+  //for (var i = 0; i < PerPage; i++) {
+  //const li = document.getElement("li");
+  //const a = document.getElement("a");
+  // a.textContext = i;
+  //li.appendChild= document.createElement ("a");
+  // ul.appendChild(li);
+  //console.log(totalPageNum);
+  //}
+}
+//}
 
-               //console.log(totalPageNum);
-           } 
-         }       
-      };
+appendPageLinks(listItem);
 
-         
-         appendPageLinks(listItem);
-         
-      
+//button.addEventListener("click", () => {
+ //li.textContent = inputValue + ":";
+//});
 
 /**
 
@@ -173,7 +158,7 @@ The active class name should be added to the link that was just clicked. The tar
 The function to show a page should be called, passing in as arguments, the global variable for the list items, and the page number that should be shown. The text content of the A element that was just clicked can be helpful here.
 
       ***/
-     /** 
+/** 
      for (var i = 0; i < a.length; i++) {
       
       if (listDiv.style.display == 'none') {
